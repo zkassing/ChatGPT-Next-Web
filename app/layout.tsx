1c5e4e32cad4644eb1439024cca0a59202d3d37f
张有宽 <kassing@live.cn>
2023-07-15 20:34:43 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "云和数据",
+  description: "打造你个人的AI工具",
   viewport: {
     width: "device-width",
     initialScale: 1,
